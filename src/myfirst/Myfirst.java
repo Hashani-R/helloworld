79fd126a0db787b44d7d57638573ff70540d6147
Hashani Ranawake <119966875+Hashani-R@users.noreply.github.com>
2022-12-17 09:27:04 +0530

    Add files via upload

---
+++ src/myfirst/Myfirst.java
@@ -0,0 +1,13 @@
+package myfirst;
+
+public class Myfirst {
+
+  
+    public static void main(String[] args) {
+        System.out.println("My First Program.");
+        System.out.println("Hello World!");
+      
+        
+    }
+    
+}
